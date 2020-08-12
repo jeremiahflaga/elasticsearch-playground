e014f588d97532d1d4c7132d3a346410979849b8
Jeremiah Flaga <j.flaga@arcanys.com>
2020-08-12 16:08:49 +0800

    sample sort using geolocation

---
+++ 2020-08-12-elasticsearch-geolocation/Program.cs
@@ -0,0 +1,95 @@
+﻿using Nest;
+using System;
+
+namespace ElasticSearchGeolocationExample
+{
+    class Program
+    {
+        private static GeoLocation baseGeolocation = GeoLocation.TryCreate(10.330114, 123.898578);
+        static void Main(string[] args)
+        {
+            var client = CreateClient();
+            Index(client);
+            var searchResponse = Search(client);
+
+            Console.WriteLine($"People sorted by distance from ({baseGeolocation}):");
+            foreach (var item in searchResponse.Documents)
+                Console.WriteLine($"{item.Id}: {item.FirstName} {item.LastName} ({item.Location})");
+        }
+
+        private static ElasticClient CreateClient()
+        {
+            var settings = new ConnectionSettings(new Uri("http://localhost:9200"))
+                            .DefaultIndex("people");
+
+            // enable trace on your ConnectionSettings object: https://stackoverflow.com/a/28940234/1451757
+            settings.EnableDebugMode();
+            // log all requests made to Elastic: https://stackoverflow.com/a/50023531/1451757
+            settings.OnRequestCompleted(call =>
+            {
+                if (call.RequestBodyInBytes != null)
+                    System.Diagnostics.Debug.Write(System.Text.Encoding.UTF8.GetString(call.RequestBodyInBytes));
+            });
+
+            var client = new ElasticClient(settings);
+            var createIndexResponse = client.Indices.Create("people", c => c
+                .Map<Person>(p => p
+                    //automatically create the mapping from the type
+                    .AutoMap()
+                    .Properties(pr => pr.GeoPoint(g => g.Name(n => n.Location)))
+                )
+            );
+
+            return client;
+        }
+
+        private static void Index(ElasticClient client)
+        {
+            var person = new Person
+            {
+                Id = 1,
+                FirstName = "Jboy",
+                LastName = "Flaga",
+                Location = GeoLocation.TryCreate(41.12, -71.34),
+            };
+            var indexResponse = client.IndexDocument(person);
+
+            var person2 = new Person
+            {
+                Id = 2,
+                FirstName = "Jboy 222",
+                LastName = "Flaga",
+                Location = GeoLocation.TryCreate(10.317985, 123.891128),
+            };
+            var indexResponse2 = client.IndexDocument(person2);
+
+            var person3 = new Person
+            {
+                Id = 3,
+                FirstName = "Jboy 333",
+                LastName = "Flaga",
+                Location = GeoLocation.TryCreate(7.051762, 125.590022),
+            };
+            var indexResponse3 = client.IndexDocument(person3);
+        }
+
+        private static ISearchResponse<Person> Search(ElasticClient client)
+        {
+            return client.Search<Person>(s => s
+                            .From(0)
+                            .Size(10)
+                            .Query(q => q
+                                 .Match(m => m
+                                    .Field(f => f.FirstName)
+                                    .Query("Jboy")
+                                 )
+                            )
+                            .Sort(ss => ss.GeoDistance(g => g
+                                  .Field(f => f.Location)
+                                  .Order(SortOrder.Ascending)
+                                  .DistanceType(GeoDistanceType.Plane)
+                                  .Points(baseGeolocation)))
+            );
+        }
+    }
+}
---
+++ 2020-08-12-elasticsearch-geolocation/README.md
@@ -0,0 +1,15 @@
+# Sort people by geolocation distance
+
+uses ElasticSearch 7.8.1 (https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.8.1-windows-x86_64.zip)
+
+Kibana 7.8.1 (https://artifacts.elastic.co/downloads/kibana/kibana-7.8.1-windows-x86_64.zip)
+
+
+## References:
+
+[ElasticSearch & NEST: Getting Started](https://www.elastic.co/guide/en/elasticsearch/client/net-api/current/nest-getting-started.html)
+
+[How can I sort by geodistance in NEST for Elasticsearch?](https://stackoverflow.com/questions/35273788/how-can-i-sort-by-geo-distance-in-nest-for-elasticsearch)
+
+[Elastic Search .NET Core Unsuccessful Low Level Call on Post](https://stackoverflow.com/questions/52188217/elastic-search-net-core-unsuccessful-low-level-call-on-post/63370145)
+
